9fed3cfa83d49213c3976588a61fa85b396430d2
Ras-Kur <raskurenergy@gmail.com>
2022-03-14 01:12:31 +0300

    Виселица 0,0,1

---
+++ OOP/HangMancpp.cpp
@@ -0,0 +1,272 @@
+﻿/*Создайте консольную версию игры «Виселица».
+
+Важно:
+■■ Слово выбирается компьютером из списка слов.
+■■ Список слов находится в файле в зашифрованном виде.
+■■ По завершении игры на экран выводится статистика игры:
+• количество времени;
+• количество попыток;
+• искомое слово;
+• буквы игрока.
+
+
+■■ Слово выбирается компьютером из списка слов.
+-------для этого я буду использовать текстовый файл "с набором слов".
+
+■■ Список слов находится в файле в зашифрованном виде.
+-------программа преобразует все мои слова из файла "с набором слов" к шифр каждое слово
+-------после чего перепишет в новый файл "с закодированными словами"
+
+
+■■ По завершении игры на экран выводится статистика игры:
+-------игра это класс. - "class Game".
+-------каждая игра это новый обьект класса. таким образом мы сможем сохранить
+• количество времени;
+• количество попыток;
+• искомое слово;
+• буквы игрока.
+
++++++++    ОБ ИГРЕ     ++++++++++
+--------------в главном меню будет:
+- новая игра.
+- продолжить игру.
+- Выбор бонусов.
+- статистика игр и очков.
+- опции.
+- выход.
+
+----------------в опциях:
+- Выбор языка.
+- Выбор сложности.
+- титры с создателями.
+
+*/
+
+#include<iostream>
+#include<fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include<random>
+#include<stack>
+
+std::string RandomWord(bool lang); // рандом достает из файла строку
+
+std::string LevelWord(int lvl_diff, std::string text, bool lang); //сложность по кол-ву букв в слове
+
+
+class Game
+{
+	std::string g_word;	//сохранение слов
+	char user_input;//ввод пользователя
+	int size; // для количества попыток
+public:
+	Game() {};
+	bool GamePlay(std::string ng_word, int lvl_diff, bool lang) // сам движок игры или что это
+	{
+		//system("chcp 65001");
+		bool flag = false;
+		bool flag_size = false;
+		bool gameover;
+		int correct_result = 0;
+		if (lvl_diff == 1) { size = 9; }
+		if (lvl_diff == 2) { size = 8; } // сдожность влияет на кол-во попыток
+		if (lvl_diff == 3) { size = 7; }
+		std::string check; // для попыток угадать
+		std::string user_check; // пользовательский ввод статистика.
+		int r_size = ng_word.size();// что бы предлагалас одна буква
+		int r_int = rand() % r_size;// для этого же
+
+		for (size_t i = 0; i < ng_word.size(); i++)//что бы выводились "_ _ _"
+			check.push_back('_');
+
+		for (int i = 0; i < ng_word.size(); i++)
+		{
+			if (i == r_int)
+				check.at(i) = ng_word.at(i); // нижние слеши с одной буквой для вывода
+		}
+
+		do
+		{
+			system("cls");
+
+			
+
+			std::cout << "\n\t"; // вывод нижнего слеша с буквой.
+			for (size_t i = 0; i < check.size(); i++)
+				std::cout << check.at(i) << " ";
+			lang == true ?
+				std::cout << "\n\n your letters : " ://ваши буквы
+				std::cout << "\n\n ваши буквы : ";
+
+			for (int i = 0; i < user_check.size(); i++)
+				std::cout << user_check.at(i) << " ";
+			lang == true ?
+				std::cout << "\n\n try to guess the letter -> " :// 
+				std::cout << "\n\n попробуйте угадать букву -> ";
+				std::cin >> user_input;//ввод буквы пользователем
+			std::cout << "\b\n";
+			user_check.push_back(user_input);
+			
+			for (int i = 0; i < ng_word.size(); i++)
+			{
+				if (user_input == ng_word.at(i)) //если введенная буква есть в слове
+				{
+					check.at(i) = user_input;
+					std::cout << check.at(i) << "\n"; 
+					flag_size=true;
+				}
+			}
+			
+			if (!flag_size)
+				size--;
+
+			flag_size = false;
+
+			for (int i = 0; i < ng_word.size(); i++)
+			{
+				if (check.at(i)==ng_word.at(i))           
+					correct_result++;
+			}
+			if (correct_result == static_cast<int>(ng_word.size()))
+			{
+				flag = true;
+				gameover = true;
+			}
+			correct_result = 0;
+			if (size == 0)
+				flag = true;
+		} while (!flag);
+
+		if (size == 0)
+		 gameover = false; 
+
+		if (gameover==true) 
+		 std::cout << "\n WINER!!!!"; 
+		else 
+		 std::cout << "\n loser!"; 
+		
+		return flag;
+	}
+	
+};
+
+
+int main()
+{
+	
+	srand(time(NULL));
+	bool lang = true;  //lang != true ? std::cout << "ru" : std::cout << "en";
+	int lvl_difficulty = 1;
+	system("chcp 65001"); system("cls");// для вывода в консоль кириллицы
+	//std::cout << RandomWord(lang) << "\n\t";
+			  //<< LevelWord(lvl_difficulty, RandomWord(lang),lang);
+	
+
+	//Game g1; g1.GamePlay(LevelWord(lvl_difficulty, RandomWord(lang), lang), lvl_difficulty,lang);
+	lang = false;
+	lvl_difficulty = 2;
+	Game g2; g2.GamePlay(LevelWord(lvl_difficulty, RandomWord(lang), lang), lvl_difficulty,lang);
+}
+
+std::string RandomWord(bool lang) // рандом достает из файла строку
+{
+	std::mt19937 engine;			 //	два часа искал
+	engine.seed(std::time(nullptr)); // что бы был рандом
+	std::random_device device;		 // из чисел больше чем
+	engine.seed(device());			 // 32'767 - это предел для "rand()"
+
+	std::ifstream file; // создал переменную для чтения файла
+	// переменная для пути к файлу
+	if (lang)
+	{
+		std::string way_n("Word_en.txt"); // английский сборник слов
+		file.open(way_n);
+	}
+	else
+	{
+		std::string way_n("Word_ru.txt"); // русский сборник слов
+		file.open(way_n);
+	}
+	std::string text;	// для записи строки из файла в память
+		// открываю файл
+	if (!file.is_open())	//проверка на открытие файла
+		std::cout << "\n\n File not open! (Файл не открыт!)\n\n";	//если ошибка
+	else
+	{		// случайно выбираем строку со словом.
+		long long  random = lang != true ? engine() % 67774 : engine() % 3000;//русский или английский
+		for (size_t i = 0; i < random; i++)
+		{
+			std::getline(file, text);//записываем строку в переменную
+			if (i == random - 1)
+				return text;
+		}
+	}
+	file.close();
+}
+
+std::string LevelWord(int lvl_diff, std::string text, bool lang) //сложность по кол-ву букв в слове
+{
+	if (lang)  // язык влияет на количество букв в сложности
+	{  // сейчас английский( если true )
+		std::string buff = text; // сохраняем значение из рандома что бы потом вернуть
+		switch (lvl_diff) // три уровня сложности
+		{
+		case 1:
+			if (buff.size() > 1 && buff.size() < 5)//от 2 до 4 букв
+				return buff;
+			else
+			{     // если иф не отработал, то перезапускаем рандом пока не подойдет начение
+				do { buff = RandomWord(lang); } while (buff.size() < 2 || buff.size() > 4);
+				return buff;
+			} break;
+		case 2:
+			if (buff.size() > 3 && buff.size() < 7) //от 4 до 6
+				return buff;
+			else
+			{
+				do { buff = RandomWord(lang); } while (buff.size() < 4 || buff.size() > 6);
+				return buff;
+			} break;
+		case 3:
+			if (buff.size() > 6 && buff.size() < 12) // от7 до 11 - это хард левел)
+				return buff;
+			else
+			{
+				do { buff = RandomWord(lang); } while (buff.size() < 7 || buff.size() > 11);
+				return buff;
+			} break;
+		}
+	}
+	else
+	{  // тут уже русский ( если false )
+		std::string buff = text; // сохраняем значение из рандома что бы потом вернуть
+		switch (lvl_diff) // три уровня сложности
+		{
+		case 1:
+			if (buff.size() > 2 && buff.size() < 6)//от 3 до 5 букв
+				return buff;
+			else
+			{     // если иф не отработал, то перезапускаем рандом пока не подойдет начение
+				do { buff = RandomWord(lang); } while (buff.size() < 3 || buff.size() > 5);
+				return buff;
+			} break;
+		case 2:
+			if (buff.size() > 4 && buff.size() < 9) //от 5 до 8
+				return buff;
+			else
+			{
+				do { buff = RandomWord(lang); } while (buff.size() < 5 || buff.size() > 7);
+				return buff;
+			} break;
+		case 3:
+			if (buff.size() > 8 && buff.size() < 15) // от 9 до 14 - это хард левел)
+				return buff;
+			else
+			{
+				do { buff = RandomWord(lang); } while (buff.size() < 9 || buff.size() > 14);
+				return buff;
+			} break;
+		}
+	}
+}
---
+++ OOP/Task14.cpp
@@ -0,0 +1,181 @@
+﻿/*Создать имитационную модель "остановка маршрутных такси". Необходимо вводить следующую информацию:
+среднее время между появлениями пассажиров на остановке в разное время суток, среднее время между появ-
+лениями маршруток на остановке в разное время суток, тип остановки (конечная или нет). Необходимо опреде-
+лить: среднее время пребывание человека на остановке, достаточный интервал времени между приходами марш-
+руток, чтобы на остановке находилось не более N людей одновременно. Количество свободных мест в маршрутке
+является случайной величиной.
+
+Задание 2.
+Разработать приложение, имитирующее очередь печати принтера. Должны быть клиенты, посылающие запросы
+на принтер, у каждого из которых есть свой приоритет. Каждый новый клиент попадает в очередь в зависимости
+от своего приоритета. Необходимо сохранять статистику печати (пользователь, время) в отдельной очереди.
+Предусмотреть вывод статистики на экран.*/
+
+#include<iostream>
+#include<queue>
+#include<string>
+	//очередь, очередь с пиоритетами
+
+/*		// задание первое
+class Bus
+{const int size = 20;
+	std::queue<int> que;
+	que=(int size);
+	 
+public:
+	Bus()
+	{
+		std::cout <<  ;
+	}
+};
+class BusStop
+{
+	bool ending_station;// конечная станция или не конечная
+	int day_people;//количество людей
+	int day_bus;//количество маршруток
+	int day_people_time;// как часто приходят
+	int day_bus_time;//как часто приезжает
+	int night_people;//количество людей
+	int night_bus;//количество маршруток
+	int night_people_time;// как часто приходят
+	int night_bus_time;//как часто приезжает
+public:
+	BusStop() :ending_station(false), day_people(0), day_bus(0), day_people_time(0), day_bus_time(0),
+		night_people(0), night_bus(0), night_people_time(0), night_bus_time(0) {};
+	BusStop(bool e_s,int day_p_t,int day_b_t, int night_p_t, int night_b_t):BusStop() 
+	{//передаю время днем людей и автобусов и ночью сразу
+		ending_station = e_s;
+		day_people_time = day_p_t; day_bus_time = day_b_t;
+		night_people_time = night_p_t; night_bus_time = night_b_t;
+	};
+	void PrintInfo()
+	{
+		std::cout << "\n ending? - " << (ending_station == true ?  " yes " :  " no ");// булевый результат в текст.
+		std::cout << "\n day - time - people - " << day_people_time;
+		std::cout << "\n day - time - bus - " << day_bus_time << "\n night - time - people - " << night_people_time;
+		std::cout << "\n night - time - bus - " << night_bus_time << "\n";
+	}
+	
+
+//определить:
+//среднее время пребывание человека на остановке
+
+//достаточный интервал времени между приходами марш-руток,
+//чтобы на остановке находилось не более N людей одновременно.
+
+//Количество свободных мест в маршрутке
+//является случайной величиной
+
+
+};
+int main()
+{
+	BusStop bs1(true, 5, 15, 12, 30);
+	bs1.PrintInfo();
+	BusStop bs2(false, 3, 20, 25, 35);
+	BusStop bs3(false, 10, 10, 22, 20);
+	BusStop bs4(false, 7, 30, 30, 40);
+	BusStop bs5(true, 5, 17, 19, 25);
+	int n;
+	n = 7;//количество н людей.
+	int x;
+	x = 15/5;// среднее время ожидания на остановке
+	int y;
+	y = x * n;
+}*/                       //не смог победить очередь, слишком долго содел над задачей. не могу продолжать терять время.
+
+			//  Задание 2.
+class Client // клиент
+{
+	std::string query; // запрос
+	int priority;     // приоритет
+	int office;      // офис
+public:
+	Client() :query("unknown"), priority(0), office(0) {};
+	Client(std::string query, int priority, int office) :Client()
+	{
+		this->query = query; this->priority = priority; this->office = office;
+	}
+	Client(int priority, int office) :Client()
+	{
+		this->priority = priority;
+		this->office = office;
+	}
+	void ClQuerty(std::string str) { query = str; }
+	int GetOffice() { return office; }
+};
+class Printer // принтер
+{
+	friend class Client;
+	std::queue <Client> clients;
+	std::priority_queue <Client> client_query;
+	int* attached_offices; // прикрепленные офисы
+public:
+	Printer() :clients(), client_query(), attached_offices(nullptr) {};
+	Printer(int* a_o_n)
+	{
+		for (int i = 0; i < 3; i++) {attached_offices[i] = a_o_n[i];}
+	}
+	int GetPQ() { return client_query.size(); }
+	void Print()
+	{
+		for (int i = 0; i < client_query.size(); i++)
+		{
+			client_query.top(); client_query.pop();
+		}
+		
+	}
+	void Print_queue(Client query_print) // принтер ставит в очередь по приоретету
+	{
+		if ( query_print.GetOffice() == attached_offices[0] ||
+			 query_print.GetOffice() == attached_offices[1] ||
+			 query_print.GetOffice() == attached_offices[2]    ) //если привязан к офису
+		{
+			clients.push(query_print);
+			client_query.push(query_print);
+			std::cout << "\n document in queue, waiting for printing...\n";
+		}
+		else
+		{
+			std::cout << "\n  !!! user does not have access to this printer !!!\n";
+			//			 "!!! у пользователя нет доступа к данному принтеру !!!"
+		}
+		if(GetPQ()==5)
+		{
+			std::cout << "\n document sent for printing\n"; // докумерт отправлен на печать
+			Print();
+		}
+	}
+	
+};
+
+int main()
+{
+	Client people1(30, 5), people2(15, 2), people3(16, 1), people4(10, 6), people5(6, 2), people6(12, 3), people7("helo world", 25, 1),
+		people8("good-luck world", 12, 4), people9(22, 5), people10(3, 6), people11(" I am King!", 31, 1), people12(11, 6);
+	people1.ClQuerty("people1"); people2.ClQuerty("people2"); people3.ClQuerty("people3"); people4.ClQuerty("people4");
+	people5.ClQuerty("people5"); people6.ClQuerty("people6"); people7.ClQuerty("people7"); people8.ClQuerty("people8");
+	people9.ClQuerty("people9"); people10.ClQuerty("people10"); people11.ClQuerty("people11"); people12.ClQuerty("people12");
+	int* arr_pr1 = new int[3]{ 1,2,3 };
+	int* arr_pr2 = new int[3]{ 2,3,4 };
+	int* arr_pr3 = new int[3]{ 4,5,6 };
+	Printer pr1(arr_pr1), pr2(arr_pr2), pr3(arr_pr3);
+	pr1.Print_queue(people1); pr1.Print_queue(people2); pr1.Print_queue(people3); pr1.Print_queue(people4); pr1.Print_queue(people5);
+	pr1.Print_queue(people6); pr1.Print_queue(people7); pr1.Print_queue(people8); pr1.Print_queue(people9); pr1.Print_queue(people10);
+	pr1.Print_queue(people11); pr1.Print_queue(people12);
+
+	pr2.Print_queue(people1); pr2.Print_queue(people2); pr2.Print_queue(people3); pr2.Print_queue(people4); pr2.Print_queue(people5);
+	pr2.Print_queue(people6); pr2.Print_queue(people7); pr2.Print_queue(people8); pr2.Print_queue(people9); pr2.Print_queue(people10);
+	pr2.Print_queue(people11); pr2.Print_queue(people12);
+
+	pr3.Print_queue(people1); pr3.Print_queue(people2); pr3.Print_queue(people3); pr3.Print_queue(people4); pr3.Print_queue(people5);
+	pr3.Print_queue(people6); pr3.Print_queue(people7); pr3.Print_queue(people8); pr3.Print_queue(people9); pr3.Print_queue(people10);
+	pr3.Print_queue(people11); pr3.Print_queue(people12);
+
+	std::cout << "\n\n\n all print doc...\n";
+	pr1.Print(); pr2.Print(); pr3.Print();
+		
+
+	// не работает. ошибка непонятная.
+
+}
